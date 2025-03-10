e2f7a115392b92ef667f7d65d53ba4fdae0b3aa2
Wire <wirewraith@gmail.com>
2025-03-10 18:45:57 +0000

    parent sample detection is being weird

--- app/Exceptions/Sentry.php
+++ app/Exceptions/Sentry.php
@@ -10,11 +10,11 @@ class Sentry
     {
         $sampleRate = config('sentry.traces_sample_rate', null);
 
-        if ($sampleRate && $context->getParentSampled()) {
-            // If the parent transaction (for example a JavaScript front-end)
-            // is sampled, also sample the current transaction
-            return 1.0;
-        }
+        // if ($sampleRate && $context->getParentSampled()) {
+        //     // If the parent transaction (for example a JavaScript front-end)
+        //     // is sampled, also sample the current transaction
+        //     return 1.0;
+        // }
 
         // Default sample rate for all other transactions (replaces `traces_sample_rate`)
         return $sampleRate === null ? 0.0 : $sampleRate;
