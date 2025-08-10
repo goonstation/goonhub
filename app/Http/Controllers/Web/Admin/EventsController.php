b9d66d8e1410a3e34c92b3bc1e1f117edd762cb2
Wire <wirewraith@gmail.com>
2025-08-10 14:54:26 +0000

    Simpler way of getting all event models

--- app/Http/Controllers/Web/Admin/EventsController.php
+++ app/Http/Controllers/Web/Admin/EventsController.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Controllers\Web\Admin;
 
-use App\Helpers\ModelHelper;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Inertia\Inertia;
 
 class EventsController extends Controller
@@ -14,7 +14,17 @@ class EventsController extends Controller
         $eventTypes = [];
         $filterType = $request->input('filters.type');
         $filteringEvent = null;
-        $eventModels = ModelHelper::getModels('App\Models\Events');
+
+        $eventModels = collect(File::allFiles(app_path('Models/Events')))
+            ->filter(function ($item) {
+                return ! str_ends_with($item->getFilename(), 'BaseEventModel.php');
+            })
+            ->map(function ($item) {
+                $name = str_replace('.php', '', $item->getFilename());
+
+                return "App\\Models\\Events\\$name";
+            });
+
         foreach ($eventModels as $modelName) {
             $model = new $modelName;
             $tableName = $model->getTable();
