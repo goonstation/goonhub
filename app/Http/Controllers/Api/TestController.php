80152adc6db7b39b071c453cc192ddd494054d49
Wire <wirewraith@gmail.com>
2025-09-22 04:14:24 +0000

    testing if octane is messing with me

--- app/Http/Controllers/Api/TestController.php
+++ app/Http/Controllers/Api/TestController.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Enums\DiscordSettings;
 use App\Http\Controllers\Controller;
-use App\Models\DiscordSetting;
-use DiscordApi;
+use App\Services\CommonRequest;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
@@ -15,11 +13,12 @@ class TestController extends Controller
      */
     public function index(Request $request)
     {
-        $guildId = DiscordSetting::where('key', DiscordSettings::TOMATO_GUILD_ID->value)->first()?->value;
-        $guild = DiscordApi::guild($guildId);
+        $commonRequest = app(CommonRequest::class);
 
         return response()->json([
-            'message' => $guild->members()->json(),
+            'fromServerId' => $commonRequest->fromServerId(),
+            'fromServer' => $commonRequest->fromServer(),
+            'fromServerGroup' => $commonRequest->fromServerGroup(),
         ]);
     }
 }
